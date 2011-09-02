2633abd32c6cb9cda5a7c91739b79ed189baa2e5
Joshua French <osakenoyojimbo@gmail.com>
2011-09-01 20:31:14 -0500

    Add 'remaining time' to raid view

--- src/com/astrofrog/android/raidtimer/RaidEdit.java
+++ src/com/astrofrog/android/raidtimer/RaidEdit.java
@@ -8,7 +8,6 @@ import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -20,6 +19,7 @@ import android.widget.ToggleButton;
 public class RaidEdit extends Activity {
     private EditText mNameText;
     private TextView mStartDateText;
+    private TextView mTimeLeftText;
     private ToggleButton mFlagged;
     private Long mRowId;
     private Long mToonId;
@@ -31,7 +31,7 @@ public class RaidEdit extends Activity {
     private int mDay;
     private int mHour;
     private int mMinute;
- 
+
     static final int TIME_DIALOG_ID = 0;
     static final int DATE_DIALOG_ID = 1;
 
@@ -46,6 +46,7 @@ public class RaidEdit extends Activity {
 
         mNameText = (EditText) findViewById(R.id.name);
         mStartDateText = (TextView) findViewById(R.id.start_date);
+        mTimeLeftText = (TextView) findViewById(R.id.timeLeft);
         mFlagged = (ToggleButton) findViewById(R.id.flaggedToggle);
 
         Button setDate = (Button) findViewById(R.id.setDate);
@@ -69,21 +70,21 @@ public class RaidEdit extends Activity {
 
         mRowId = (savedInstanceState == null) ? null :
             (Long) savedInstanceState.getSerializable(RaidsDbAdapter.KEY_ROWID);
-		if (mRowId == null) {
-			Bundle extras = getIntent().getExtras();
-			mRowId = extras.containsKey(RaidsDbAdapter.KEY_ROWID) ? extras.getLong(RaidsDbAdapter.KEY_ROWID)
-									: null;
-		}
-		
+        if (mRowId == null) {
+            Bundle extras = getIntent().getExtras();
+            mRowId = extras.containsKey(RaidsDbAdapter.KEY_ROWID) ? extras.getLong(RaidsDbAdapter.KEY_ROWID)
+                                    : null;
+        }
+
         mToonId = (savedInstanceState == null) ? null :
             (Long) savedInstanceState.getSerializable(RaidsDbAdapter.KEY_TOON_ID);
-		if (mToonId == null) {
-			Bundle extras = getIntent().getExtras();
-			mToonId = extras.containsKey(RaidsDbAdapter.KEY_TOON_ID) ? extras.getLong(RaidsDbAdapter.KEY_TOON_ID)
-									: null;
-		}
+        if (mToonId == null) {
+            Bundle extras = getIntent().getExtras();
+            mToonId = extras.containsKey(RaidsDbAdapter.KEY_TOON_ID) ? extras.getLong(RaidsDbAdapter.KEY_TOON_ID)
+                                    : null;
+        }
 
-		populateFields();
+        populateFields();
 
         confirmButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -91,32 +92,78 @@ public class RaidEdit extends Activity {
                 finish();
             }
         });
-        
+
         completeButton.setOnClickListener(new View.OnClickListener() {
-        	public void onClick(View view) {
-        		calendarSetup();
-        		updateDisplay();
-        	}
+            public void onClick(View view) {
+                calendarSetup();
+                updateDisplay();
+            }
         });
     }
 
     private void populateFields() {
-    	int name_column;
+        int name_column;
         if (mRowId != null) {
             Cursor raid = mDbHelper.fetchRaid(mRowId);
             startManagingCursor(raid);
             if(raid.getColumnIndex(RaidsDbAdapter.KEY_NAME) != -1) {
-            	name_column = raid.getColumnIndex(RaidsDbAdapter.KEY_NAME);
-            	mNameText.setText(raid.getString(name_column));
+                name_column = raid.getColumnIndex(RaidsDbAdapter.KEY_NAME);
+                mNameText.setText(raid.getString(name_column));
             }
             mStartDateText.setText(raid.getString(
-            		raid.getColumnIndexOrThrow(RaidsDbAdapter.KEY_START_DATE)));
+                    raid.getColumnIndexOrThrow(RaidsDbAdapter.KEY_START_DATE)));
             if(raid.getInt(raid.getColumnIndex(RaidsDbAdapter.KEY_FLAGGED)) == 1) {
-            	mFlagged.setChecked(true);
+                mFlagged.setChecked(true);
             } else {
-            	mFlagged.setChecked(false);
+                mFlagged.setChecked(false);
+            }
+
+            if(dateMatcher(mStartDateText.getText().toString())) {
+                calculateRemainingTime();
             }
         }
+    }
+
+    private boolean dateMatcher(String date) {
+        return date.matches("^\\d{2}-\\d{2}-\\d{4} \\d{2}:\\d{2}$");
+    }
+
+    // TODO there is still some wonkiness on when this method updates the field, not really sure why it sometimes takes double clicks to work
+    private void calculateRemainingTime() {
+        Calendar now = Calendar.getInstance();
+        Calendar lastRan = Calendar.getInstance();
+        long days, hours, minutes;
+
+        stringDateToCalendar(lastRan);
+
+        long nowWithoutMillis = now.getTime().getTime() / 1000;
+        long lastRanWithoutMillis = lastRan.getTime().getTime() / 1000;
+
+        long nextRun = lastRanWithoutMillis + (86400*2 + 3600*18); // add 2 days and 18 hours since that is "wait time"
+        long remainingTimeInSeconds = nextRun - nowWithoutMillis;
+        if(remainingTimeInSeconds < 0) {
+            mTimeLeftText.setText("Ready to repeat!");
+        } else {
+            /* days hours minutes breakout */
+            days = remainingTimeInSeconds / 86400;
+            remainingTimeInSeconds %= 86400;
+            hours = remainingTimeInSeconds / 3600;
+            remainingTimeInSeconds %= 3600;
+            minutes = remainingTimeInSeconds / 60;
+
+            mTimeLeftText.setText("You can replay this raid in " + Long.toString(days) + " days, " + Long.toString(hours) + "hours, " + Long.toString(minutes) + "minutes.");
+        }
+    }
+
+    private void stringDateToCalendar(Calendar c) {
+        String date = mStartDateText.getText().toString();
+        int month = Integer.parseInt(date.substring(0, 2)) - 1;
+        int day = Integer.parseInt(date.substring(3, 5));
+        int year = Integer.parseInt(date.substring(6, 10));
+        int hour = Integer.parseInt(date.substring(11, 13));
+        int minute = Integer.parseInt(date.substring(14, 16));
+
+        c.set(year, month, day, hour, minute);
     }
 
     private void calendarSetup() {
@@ -170,8 +217,8 @@ public class RaidEdit extends Activity {
                 return new DatePickerDialog(this,
                         mDateSetListener, mYear, mMonth, mDay);
             case TIME_DIALOG_ID:
-            	return new TimePickerDialog(this,
-            			mTimeSetListener, mHour, mMinute, false);
+                return new TimePickerDialog(this,
+                        mTimeSetListener, mHour, mMinute, false);
         }
         return null;
     }
@@ -183,20 +230,23 @@ public class RaidEdit extends Activity {
                 ((DatePickerDialog) dialog).updateDate(mYear, mMonth, mDay);
                 break;
             case TIME_DIALOG_ID:
-            	((TimePickerDialog) dialog).updateTime(mHour, mMinute);
-            	break;
+                ((TimePickerDialog) dialog).updateTime(mHour, mMinute);
+                break;
         }
     }
 
     private void updateDisplay() {
+        if(dateMatcher(mStartDateText.getText().toString())) {
+            calculateRemainingTime();
+        }
         mStartDateText.setText(
                 new StringBuilder()
-                        // Month is 0 based so add 1
-                        .append(mMonth + 1).append("-")
-                        .append(mDay).append("-")
-                        .append(mYear).append(" ")
-                        .append(pad(mHour)).append(":")
-                		.append(pad(mMinute)));
+                .append(pad(mMonth + 1)).append("-")
+                .append(pad(mDay)).append("-")
+                .append(mYear).append(" ")
+                .append(pad(mHour)).append(":")
+                .append(pad(mMinute)));
+
     }
 
     private DatePickerDialog.OnDateSetListener mDateSetListener =
@@ -210,16 +260,16 @@ public class RaidEdit extends Activity {
                 updateDisplay();
             }
         };
-        
+
     private TimePickerDialog.OnTimeSetListener mTimeSetListener =
-    	new TimePickerDialog.OnTimeSetListener() {
-			
-			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-				mHour = hourOfDay;
-				mMinute = minute;
-				updateDisplay();
-			}
-		};
+        new TimePickerDialog.OnTimeSetListener() {
+
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                mHour = hourOfDay;
+                mMinute = minute;
+                updateDisplay();
+            }
+        };
 
     private static String pad(int c) {
         if (c >= 10)
